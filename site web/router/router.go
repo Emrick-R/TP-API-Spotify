49707704c67e1714478ff171925243deb4eb7e1c
Emrick <emrick.rivet@ynov.com>
2025-11-20 11:50:26 +0100

    premier commit

---
+++ site web/router/router.go
@@ -0,0 +1,21 @@
+package router
+
+import (
+	"TP-API-Spotify/controller"
+	"net/http"
+)
+
+// New crée et retourne un nouvel objet ServeMux configuré avec les routes de l'application
+func New() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	// Routes de ton app
+	mux.HandleFunc("/", controller.Home)
+	mux.HandleFunc("/contact", controller.Contact)
+
+	// Ajout des fichiers statiques
+	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+	return mux
+}
